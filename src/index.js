73cf9da5dc7fab6acae7926ee71d4a0bd836b71d
Oscar <ofgonzalez87@gmail.com>
2021-02-28 19:58:55 -0400

    Commit 171

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {HeroesApp} from "./HeroesApp.js";
+import { HeroesApp } from "./HeroesApp.js";
 
 ReactDOM.render(<HeroesApp />, document.getElementById("root"));

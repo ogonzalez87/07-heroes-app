7f3e8e3a50ff471418b7e7cec6a33f8e8d48521c
Oscar <ofgonzalez87@gmail.com>
2021-02-28 20:21:07 -0400

    Commit 172 segundo router

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -6,7 +6,7 @@ export const Navbar = () => {
     <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
       <Link className="navbar-brand" to="/">
         Asociaciones
-      </Link> 
+      </Link>
 
       <div className="navbar-collapse">
         <div className="navbar-nav">
@@ -30,8 +30,8 @@ export const Navbar = () => {
         </div>
       </div>
 
-      <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
-        <ul className="navbar-nav ml-auto">
+      <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end ">
+        <ul className="navbar-nav ml-auto ">
           <NavLink
             activeClassName="active"
             className="nav-item nav-link"
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,18 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { LoginScreen } from "../components/login/LoginScreen";
-import { Marvel } from "../components/marvel/Marvel";
-import { Navbar } from "../components/ui/Navbar";
+import { MarvelScreen } from "../components/marvel/MarvelScreen";
+import { DashBoardRouters } from "./DashBoardRouters";
+// import { Navbar } from "../components/ui/Navbar";
 
 export const AppRouter = () => {
   return (
     <Router>
       <div>
-        <Navbar />
-
         <Switch>
           <Route exact path="/login" component={LoginScreen} />
-          <Route exact path="/" component={Marvel} />
+          <Route path="/" component={DashBoardRouters} />
         </Switch>
       </div>
     </Router>

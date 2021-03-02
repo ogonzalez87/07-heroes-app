23875e8e3f4ff48268bb17d24ac072627f54140e
Oscar <ofgonzalez87@gmail.com>
2021-03-01 20:55:57 -0400

    Commit 176

--- src/components/heroes/HeroScreen.js
+++ src/components/heroes/HeroScreen.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import { Redirect, useParams } from "react-router-dom";
+import { getHeroesById } from "../../selectors/getHeroById";
 
 export const HeroScreen = () => {
+  const { heroeId } = useParams();
+  const hero = getHeroesById(heroeId);
+
+  if (!hero) {
+    return <Redirect to="/" />;
+  }
+  const {
+    id,
+    superhero,
+    publisher,
+    alter_ego,
+    first_appearance,
+    characters,
+  } = hero;
   return (
     <div>
       <h1>HeroScreen</h1>
     </div>
-  )
-}
+  );
+};
--- src/routers/DashBoardRouters.js
+++ src/routers/DashBoardRouters.js
@@ -12,7 +12,7 @@ export const DashBoardRouters = () => {
       <div className = 'container mt-2' >
         <Switch>
           <Route exact path="/marvel/" component={MarvelScreen} />
-          <Route exact path="/heroe/:hero/:heroeId" component={HeroScreen} />
+          <Route exact path="/hero/:heroeId" component={HeroScreen} />
           <Route exact path="/dc/" component={DcScreen} />
           <Redirect to="/marvel/" />
         </Switch>

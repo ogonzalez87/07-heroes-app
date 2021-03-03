13411b81cbe5b0a0e2a527ffe0b4c81ce3739e15
Oscar <ofgonzalez87@gmail.com>
2021-03-03 05:04:24 -0400

    Commit 181

--- README.md
+++ README.md
@@ -10,3 +10,7 @@ npm install react-router-dom
     rel="stylesheet"
     href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
   />
+
+  ####
+  npm i query-string
+  
--- src/components/search/SearchScreen.js
+++ src/components/search/SearchScreen.js
@@ -1,24 +1,25 @@
 import React from "react";
+import queryString from "query-string";
+import { useLocation } from "react-router-dom";
 import { heroes } from "../../data/heroes";
 import { useForm } from "../../hooks/useForm";
 import { HeroCard } from "../heroes/HeroCard";
 
-export const SearchScreen = () => {
+export const SearchScreen = ({ history }) => {
   const heroesFiltered = heroes;
+  const location = useLocation();
 
-  // const initialForm = {
-  //   name: "",
-  //   age: 0,
-  //   email: "",
-  // };
+  const { q = "" } = queryString.parse(location.search);
+
   const [formValues, handleInputChange] = useForm({
-    searchText: "",
+    searchText: q,
   });
 
   const { searchText } = formValues;
 
   const handleSearch = (e) => {
     e.preventDefault();
+    history.push(`?q=${searchText}`);
     console.log(searchText);
   };
 
@@ -38,7 +39,7 @@ export const SearchScreen = () => {
               className="form-control"
               name="searchText"
               value={searchText}
-              autoComplete= 'off'
+              autoComplete="off"
               onChange={handleInputChange}
             />
             <button type="submit" className="btn m-1 btn-block ">

eda9c2d8beac6df850f9bff16c226139e81a7aa7
Oscar <ofgonzalez87@gmail.com>
2021-03-04 06:36:14 -0400

    commit 190

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -1,10 +1,22 @@
 import React, { useContext } from "react";
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, useHistory } from "react-router-dom";
 import { AuthContext } from "../../auth/AuthContext";
+import { types } from "../../types/types";
 export const Navbar = () => {
-  const {user:{name}} = useContext(AuthContext) ;
+  const {
+    user: { name },
+    dispatch,
+  } = useContext(AuthContext);
 
-  // console.log(name) ;
+  const history = useHistory();
+
+  const handleLogout = () => {
+    history.replace("./login");
+    dispatch({
+      type: types.logout,
+    });
+  };
+
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
       <Link className="navbar-brand" to="/">
@@ -45,14 +57,9 @@ export const Navbar = () => {
       <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end ">
         <ul className="navbar-nav ml-auto ">
           <span className="nav-item nav-link text-info "> {name}</span>
-          <NavLink
-            activeClassName="active"
-            className="nav-item nav-link"
-            exact
-            to="/login"
-          >
+          <div className="nav-item nav-link  btn" onClick={handleLogout}>
             Logout
-          </NavLink>
+          </div>
         </ul>
       </div>
     </nav>
